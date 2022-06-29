5c178e47e754fed32770915308c80c4463474c12
Nikhil Sharma <nikhilsharmapro19@gmail.com>
2022-06-29 18:56:58 +0530

    Completed Components of Show.js

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,6 +1,10 @@
 import React,{useReducer,useEffect} from 'react'
 import { useParams } from 'react-router-dom'
 import { apiGet } from '../misc/config'
+import ShowMainData from '../components/show/ShowMainData'
+import Details from '../components/show/Details'
+import Seasons from '../components/show/Seasons'
+import Cast from '../components/show/Cast'
 
 const Reducer = (prevState, action) => {
     const val=action.type;
@@ -46,7 +50,7 @@ const Show = () => {
   }, [id])
 
 
-  console.log(show);
+  //console.log(show);
   if(isLoading){
     return <div>Data is being Loaded!</div>
   }
@@ -57,7 +61,35 @@ const Show = () => {
 
   return (
     <div>
-        HELLO
+        <ShowMainData
+            image={show.image}
+            name={show.name}
+            rating={show.rating}
+            summary={show.summary}
+            tags={show.genres}
+        />
+        <div>
+            <h2>Details</h2>
+            <Details
+                status={show.status}
+                network={show.network}
+                premiered={show.premiered}
+            />
+        </div>
+
+        <div>
+            <h2>Seasons</h2>
+            <Seasons
+                seasons={show._embedded.seasons}
+            />
+        </div>
+
+        <div>
+            <h2>Cast</h2>
+            <Cast
+                cast={show._embedded.cast}
+            />
+        </div>
     </div>
   )
 }
