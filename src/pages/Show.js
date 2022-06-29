c01964d33ea60928dec455f30eea7243923401bd
Nikhil Sharma <nikhilsharmapro19@gmail.com>
2022-06-29 16:44:56 +0530

    Loading before spinner added

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -5,15 +5,41 @@ import { apiGet } from '../misc/config'
 
 const Show = () => {
   const {id} =  useParams(); 
-  const [show,setShow] = useState('null')
+  const [show,setShow] = useState('null');
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(()=>{
+    let isMounted=true;
     apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`).then(results => {
-        setShow(results);
+        setTimeout(()=>{
+            if(isMounted){
+                setShow(results);
+                setIsLoading(false);
+            }
+        },2000)
+    }).catch(err => {
+        if(isMounted){
+            setIsLoading(false);
+            setError(err);
+        }
     })
+
+    return () => {
+        isMounted=false;
+    }
+
   }, [id])
 
   console.log(show);
+
+  if(isLoading){
+    return <div>Data is being Loaded!</div>
+  }
+
+  if(error){
+    return <div>`Error: ${error}`</div>
+  }
 
   return (
     <div>
